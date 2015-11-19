81eba24f2c53dd6d916ccfa1f2d12bdc10eaaf49
mikereddystudio <mikereddystudio@gmail.com>
2015-11-19 09:43:49 -0600

    working final project

---
+++ unit_10/javascript/splash.js
@@ -0,0 +1,12 @@
+var images = [
+    'splash1.jpg',  
+    'splash2.jpg', 
+    'splash3.jpg'];
+    
+$('<img class="splash-imgs" src="../working/images/splash/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#splash');
+
+
+
+//		the current file path for images is:  "../splashrotate/images/splash/splash1.jpg"
+//		this will probably change in the future, so pay attention to line 6 in the js file
+
